e479b4e1071fb9ef4c6ab0844bf535c9a05bed92
MR MACHINE <93038433+Mrmachinepro@users.noreply.github.com>
2022-09-16 17:01:28 -0700

    Add files via upload

---
+++ facebookreporter.py
@@ -0,0 +1,180 @@
+import requests 
+import time 
+import colorama
+from colorama import Fore, Back ,Style
+import os
+
+os.system("clear")
+time.sleep(1)
+
+print (Fore.GREEN + "report tool started ...")
+time.sleep(1)
+
+os.system("clear")
+
+slaw0= '''
+________________________________________________________________________________
+'''
+print(Fore.YELLOW + slaw0)
+
+slaw = '''
+	 ____________________________________________________________
+	| __  __ ____    __  __    _    ____ _   _ ___ _   _ _____   |
+	| |  \/  |  _ \  |  \/  |  / \  / ___| | | |_ _| \ | | ____| |
+	| | |\/| | |_) | | |\/| | / _ \| |   | |_| || ||  \| |  _|   |
+	| | |  | |  _ < _| |  | |/ ___ \ |___|  _  || || |\  | |___  |
+	| |_|  |_|_| \_(_)_|  |_/_/   \_\____|_| |_|___|_| \_|_____| |
+	|____________________________________________________________|
+'''
+print(Fore.RED + slaw)
+
+slaw2 = '''
+		* * * * * * * * * * * * * * * * * * * * *
+		* [#]  welcome to facebook report tool  *
+		*                                       *
+		* [#]  created : mr machine             * 
+		*                                       *
+		* [#] try to use it for ethical purpose *
+		* * * * * * * * * * * * * * * * * * * * *
+	'''
+print(Fore.GREEN + slaw2)
+	
+slaw3 = '''
+__________________________________________________________________________________
+	'''
+print(Fore.BLUE + slaw3)
+
+#00000000000000000000000000000000000000000000000000   functionakan    0000000000000000000000000000000000000000000000000000000000
+
+
+fb =input(Fore.YELLOW + '[+] Please inter the facebook URL : ')
+time.sleep(2)
+
+os.system("clear")
+
+print("report automation is startig.....")
+
+url = 'https://m.facebook.com/help/contact/209046679279097'
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print(Fore.RED + '[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+
+data = {
+'crt-url' : fb,
+'cf_age':"less than 9 years",
+'submit':'submit'
+}
+whisper=requests.post(url,data=data)
+print('[+]report done....')
+time.sleep(2)
+
+print (Fore.BLUE + "successfully we reported the facebook URL ...")
